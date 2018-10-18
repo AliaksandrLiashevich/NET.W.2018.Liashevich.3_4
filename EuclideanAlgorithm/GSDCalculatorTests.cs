e032097e192e3cdd17112797b0e1eba1073f04b1
AliaksandrLiashevich <43993109+AliaksandrLiashevich@users.noreply.github.com>
2018-10-18 18:03:01 +0300

    Update EuclideanAlgorithm/GSDCalculatorTests.cs

--- EuclideanAlgorithm/GSDCalculatorTests.cs
+++ EuclideanAlgorithm/GSDCalculatorTests.cs
@@ -13,7 +13,7 @@ namespace NET.W._2018.Liashevich._3_4.Tests
         [TestCase(new int[] { 5 }, 5)]
         public void EuclideanAlgorithm_15and10and20_5returned(int[] numbers, int expected)
         {
-            Assert.AreEqual(expected, SearchGCD.EuclideanSearch(numbers));
+            Assert.AreEqual(expected, GSDCalculator.EuclideanAlgorithm(numbers));
         }
 
         [TestCase(new int[] { 15, 10, 20 }, 5)]
@@ -22,7 +22,7 @@ namespace NET.W._2018.Liashevich._3_4.Tests
         [TestCase(new int[] { 1 }, 1)]
         public void BinaryEuclideanAlgorithm_15and10and20_5returned(int[] numbers, int expected)
         {
-            Assert.AreEqual(expected, SearchGCD.BinaryEuclideanSearch(numbers));
+            Assert.AreEqual(expected, GSDCalculator.BinaryEuclideanAlgorithm(numbers));
         }
     }    
 }
